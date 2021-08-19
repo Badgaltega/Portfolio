b549b89b6ef829b964beece191c415d5c586d1cc
Oteri Tega <oterit88@gmail.com>
2021-08-19 05:39:56 -0700

    Worked on the send_message logic

--- resume/views.py
+++ resume/views.py
@@ -9,14 +9,19 @@ def home(request):
 
 
 def send_message(request):
+
+    # Gets value from the form and saves them to their respective variable
     name = request.POST.get("name")
     email = request.POST.get("email")
     subject =  request.POST.get("subject")
     message = request.POST.get("message")
 
+    # Creates a variable named save_message and instantiate the Message model class to it
     save_message = Message(
         name=name, email=email, subject=subject, message=message
     )
+
+    # Saves the data that will be gotten from the form
     save_message.save()
     messages.success(request, "Yayy! Your message has been submitted.")
 
