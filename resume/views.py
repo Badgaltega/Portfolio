fe3adad9024a374679c2bd16260d33a0bc238d27
Oteri Tega <oterit88@gmail.com>
2021-08-19 03:44:30 -0700

    Update resume views

--- resume/views.py
+++ resume/views.py
@@ -1,5 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from resume.models import Message
+
 
 def home(request):
     context = {}
     return render(request, "resume/index.html", context)
+
+
+def send_message(request):
+    name = request.POST.get("name")
+    email = request.POST.get("email")
+    subject =  request.POST.get("subject")
+    message = request.POST.get("message")
+
+    save_message = Message(
+        name=name, email=email, subject=subject, message=message
+    )
+    save_message.save()
+    messages.success(request, "Yayy! Your message has been submitted.")
+
+    return redirect("resume:home")
